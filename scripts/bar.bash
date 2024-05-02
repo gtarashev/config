3d3e3e000e0fed679bb8a0a0dce54a5c42b9dfd9
Georgi Tarashev <georgitarashev@gmail.com>
2024-05-02 15:42:26 +0100

    updated bash scripts in ./scripts to follow the stylesheet

--- scripts/bar.bash
+++ scripts/bar.bash
@@ -6,15 +6,15 @@ while :; do
     # update some things only once a min rather than once a second
     if [[ $(expr $counter % 60) == 0 ]] ; then
         # get the date and the time (see `man date`)
-	    curr_date="$(date +'%A (%-e/%-m/%Y)')"
-	    curr_time="$(date +'%-l:%M %p')"
+	    curr_date=$(date +'%A (%-e/%-m/%Y)')
+	    curr_time=$(date +'%-l:%M %p')
 	    # get the battery percentage (see `man upower` then grep and awk to format)
-	    battery="$(upower -i `upower -e | grep 'BAT'` | grep 'percentage' | awk '{ print $2 }')"
+	    battery=$(upower -i `upower -e | grep 'BAT'` | grep 'percentage' | awk '{ print $2 }')
     fi
 
     # update once every 15 minutes (do not want to constantly send curl requests to wttr.in)
     if [[ $(expr $counter % 900) == 0 ]] ; then
-      weather="$(curl -s wttr.in/burgas?format='%c+%t+(%f)')"
+      weather=$(curl -s wttr.in/burgas?format='%c+%t+(%f)')
 	    # reset counter
 	    counter=0
     fi
@@ -22,7 +22,7 @@ while :; do
     
     # set xsetroot name ($DISPLAY is an environment variable declared by the system when
     # an xsession is started)
-    printing=`echo "$weather | 🔋 $battery | $curr_date  🕰 $curr_time" | sed 's/\xef\xd8\x8f//g'`
+    printing=$(echo "$weather | 🔋 $battery | $curr_date  🕰 $curr_time" | sed 's/\xef\xd8\x8f//g')
 
     xsetroot -d "$DISPLAY" -name "$printing"
     # update counter
