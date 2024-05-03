91f1ce401a631c0dd40715eb67e24117fd6c9bab
Georgi Tarashev <georgitarashev@gmail.com>
2024-05-03 13:25:08 +0100

    removed duplicate sym links from install script

--- install-flie-TODO.md
+++
@@ -1,4 +0,0 @@
-* currently creates sym link in directory if it already exists (as link or otherwise)
-* check dependencies for st/dwm
-* make st/dwm
-* download fonts required (or make sure they are present)
--- install.bash
+++ install.bash
@@ -6,43 +6,41 @@
 varresponse=""
 
 while true; do
-    read -p "Would you like to setup sym links? [Y/n]: " varresponse
+    read -p 'Would you like to setup sym links? [Y/n]: ' varresponse
 
     if [[ -z $varresponse || $varresponse == [Yy] ]]; then
-		echo ""
+		echo ''
 
 		# make sure config exists
-		echo -n "Creating ~/.config... "
+		echo -n 'Creating ~/.config... '
 		mkdir -p ~/.config
-		echo "DONE"
+		echo 'DONE'
 
 		# get current wd 
 		dir=$(pwd)
 		# loop through directories and create links
 		echo 'Creating Sym Links:'
 		for i in $dir/dotfiles/config/*; do
-			dirname="${i##*/}"
+			dirname=${i##*/}
 			echo -n "Creating sym link for $dirname... "
-			ln -fLs $dir/dotfiles/config/$dirname $HOME/.config/$dirname || { echo "Error when creating sym link for $dirname"; exit; }
-			echo DONE
+			ln -fLs $dir/dotfiles/config/$dirname $HOME/.config/ && echo 'DONE' || echo "Error when creating sym link for $dirname"
 		done
 
-		echo ""
-		echo "Setting up special sym links:"
+		echo ''
+		echo 'Setting up special sym links:'
 		# create special sym links
-		echo -n "Setting up xinitrc... "
-		ln -fLs $dir/dotfiles/xinitrc ~/.xinitrc && echo "DONE" || echo "FAILED"
-		echo -n "Setting up bashrc... "
-		ln -fLs $dir/dotfiles/bashrc ~/.bashrc && echo "DONE" || echo "FAILED"
-		echo -n "Setting up zshrc... "
-		echo "source ~/.config/zsh/zshrc" >> ~/.zshrc && echo "DONE" || echo "FAILED"
+		echo -n 'Setting up xinitrc... '
+		ln -fLs $dir/dotfiles/xinitrc ~/.xinitrc && echo 'DONE' || echo 'FAILED'
+		echo -n 'Setting up bashrc... '
+		ln -fLs $dir/dotfiles/bashrc ~/.bashrc && echo 'DONE' || echo 'FAILED'
+		echo -n 'Setting up zshrc... '
+		echo "source $HOME/.config/zsh/zshrc" >> ~/.zshrc && echo 'ONE' || { echo 'FAILED'; break; }
 
 		break
 
-    elif [[ "$varresponse" == [Nn] ]]; then
-	echo "No"
-	break
+    elif [[ $varresponse == [Nn] ]]; then
+		break
     else
-	echo "Incorrect Choice, Please Try Again."
+		echo 'Incorrect Choice, Please Try Again.'
     fi
 done
