4a6f81a1d8fdc729dde8da0d6054f31880aef39d
Georgi Tarashev <georgitarashev@gmail.com>
2024-07-13 11:23:19 +0300

    Added absolute path to emoji and removed newline
    1. Get the directory of where the script is located
    2. Use the absolute path to emoji.txt over the relative one
    3. Remove newline from emoji before copying

--- scripts/emoji_selector.sh
+++ scripts/emoji_selector.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cat ./emoji.txt | dmenu -l 30 -fn "Oxygen Mono:size=16" -fn "AppleColorEmoji:pixelsize=16:antialias=true:autohint=true" | cut -d " " -f 1 | xclip -i
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cat $SCRIPT_DIR/emoji.txt | dmenu -l 30 -fn "Oxygen Mono:size=16" -fn "AppleColorEmoji:pixelsize=16:antialias=true:autohint=true" | cut -d " " -f 1 | tr -d "\n" | xclip -i
