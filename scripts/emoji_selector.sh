1864145857a72e2b432f44aabd277f9d858230c7
Georgi Tarashev <georgitarashev@gmail.com>
2024-07-21 15:48:40 +0300

    Updated the display-manager check
    I learned that rather than formatting the output of a command and
    comparing it, instead `$XDG_SESSION_TYPE` just gives me what I want.

--- scripts/emoji_selector.sh
+++ scripts/emoji_selector.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 to_copy=$(cat $SCRIPT_DIR/emoji.txt | dmenu -l 30 -fn "Oxygen Mono:size=16" -fn "AppleColorEmoji:pixelsize=16:antialias=true:autohint=true" | cut -d " " -f 1 | tr -d "\n")
-if [[ "$(loginctl show-session 1 -p Type | cut -d '=' -f 2)" == "wayland" ]]; then
+if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
     wl-copy -p $to_copy
 else
     xclip -o $to_copy
