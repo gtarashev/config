a0a72cd82aef622024ae867881c957c8732b93c0
Georgi Tarashev <georgitarashev@gmail.com>
2024-07-21 12:46:53 +0300

    Added ability to copy to wayland clipboard with wl-copy

--- scripts/emoji_selector.sh
+++ scripts/emoji_selector.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cat $SCRIPT_DIR/emoji.txt | dmenu -l 30 -fn "Oxygen Mono:size=16" -fn "AppleColorEmoji:pixelsize=16:antialias=true:autohint=true" | cut -d " " -f 1 | tr -d "\n" | xclip -i
+to_copy=$(cat $SCRIPT_DIR/emoji.txt | dmenu -l 30 -fn "Oxygen Mono:size=16" -fn "AppleColorEmoji:pixelsize=16:antialias=true:autohint=true" | cut -d " " -f 1 | tr -d "\n")
+if [[ "$(loginctl show-session 1 -p Type | cut -d '=' -f 2)" == "wayland" ]]; then
+    wl-copy -p $to_copy
+else
+    xclip -o $to_copy
+fi
